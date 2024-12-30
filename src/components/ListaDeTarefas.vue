ec5c4c120901415f401f67170e27104f1507bfc4
GeovaneKlinsman <geovanegondim91@gmail.com>
2024-12-30 20:35:42 -0300

    corrigindo escrita

--- src/components/ListaDeTarefas.vue
+++ src/components/ListaDeTarefas.vue
@@ -3,7 +3,7 @@ const props = defineProps(['tarefas']);
 </script>
 
 <template>
-    <ul class="list-group mt-4">
+    <ul class="list-group mt-4" v-if="props.tarefas.length > 0" >
         <li class="list-group-item" v-for="tarefa in props.tarefas">
             <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
                 :id="tarefa.titulo" type="checkbox">
@@ -12,10 +12,11 @@ const props = defineProps(['tarefas']);
             </label>
         </li>
     </ul>
+    <p v-else class="mt-4">Não há tarefas pendentes.</p>
 </template>
 
 <style scoped>
-    .done {
-        text-decoration: line-through;
-    }
+.done {
+    text-decoration: line-through;
+}
 </style>
